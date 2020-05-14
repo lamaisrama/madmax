125ba8e52cc8bfa56a13763cdf8a8918279f7bf7
alsgu <alsgu@DESKTOP-5AH91C9>
2020-05-14 15:46:13 +0900

    # mh
    로그인 회원가입 jsp추가

--- madmax/src/main/java/com/madmax/stool/user/controller/UserController.java
+++ madmax/src/main/java/com/madmax/stool/user/controller/UserController.java
@@ -1,5 +1,21 @@
 package com.madmax.stool.user.controller;
 
-public class UserController {
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
 
+import com.madmax.stool.user.model.service.UserService;
+
+@Controller
+public class UserController {
+	
+	@Autowired
+	private UserService service;
+
+	@RequestMapping("/user/joinUser.do")
+	public String joinUser() {
+		
+		return "redirect:/views/user/joinUser";
+	}
+	
 }
