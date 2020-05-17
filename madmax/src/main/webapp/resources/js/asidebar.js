8aae9fb6f277dbc43f452a4f5d7c0b1b6cd96f4c
82106 <82106@DESCKTOP-SIEUN>
2020-05-17 20:20:35 +0900

    오른쪽 사이드바, 전체 프로젝트 리스트 페이지 수정-se

--- madmax/src/main/webapp/resources/js/asidebar.js
+++ madmax/src/main/webapp/resources/js/asidebar.js
@@ -1,6 +1,26 @@
+/*파일함의 파일명으로 검색하기*/
+$(document).ready(function(){
+	$("#searchFileName").on("keyup", function() {
+		var value = $(this).val().toLowerCase();
+			$("#fileListTable tr").filter(function() {
+			console.log($(this).children().eq(0).text());
+			$(this).toggle($(this).children().eq(0).text().toLowerCase().indexOf(value) > -1)
+		});
+	});
+});
 
-/*프로젝트 참여자 전체보기 클릭 후 (나)에 대한 버튼*/
+/*전체보기> 참여자 이름으로 검색하기*/
+$(document).ready(function(){
+    $("#searchMember").on("keyup", function() {
+      var value = $(this).val().toLowerCase();
+        $(".pjJoinAllMemberList").filter(function() {
+          console.log($(this).text());
+          $(this).toggle($(this).text().toLowerCase().indexOf(value) > -1)
+        });
+    });
+  });
 
+/*프로젝트 참여자 전체보기 클릭 후 (나)에 대한 나가기 버튼*/
 $(document).ready(function() {
 	$('[data-toggle="tooltip"]').tooltip();
 });
@@ -8,8 +28,9 @@ $(document).ready(function() {
 function exitFunction() {
 	var exitBtn = document.getElementById("exitBtn");
 	if (confirm("프로젝트에서 나가시겠습니까?")) {
-
+		/*확인 누르면 프로젝트에서 나간다*/
 	} else {
+		/*취소 누르면 취소임~~~~~*/
 	}
 
 }
